76f29e252285c8091547c2a8f07e895493ddcb6d
Kenny R. Wong <99190650+KennyWong0701@users.noreply.github.com>
2022-05-30 07:55:53 -0600

    Add files via upload

---
+++ Tarea2.js
@@ -0,0 +1,33 @@
+
+var lista = ['Primero', 'Segundo', 'Tercero', 'Cuarto'];
+
+var edades = [10,15,16,17,3];
+
+var centroamerica = ['Panama','Costa Rica','Honduras','Salvador','Guatemala','Nicaragua','Belice']
+
+//Array.shift elimina el primer elemento de una lista, ademas lo devuelve como una variable
+
+//var borrar = lista.shift();
+
+//console.log(lista);
+//console.log(borrar);
+
+
+//Array.slice imprime todos los elementos de la lista en una variable
+
+var valores = lista.values();
+
+//console.log(valores.next().value);
+
+
+//Array.some, este verifica si alguno de los elementos de la lista cumple con una condicion
+
+//edades.some(mayores);
+//function mayores(edad) {
+//  return edad > 18;
+//}
+
+
+//Array.sort, esta realiza un ordenamiento de la lista, tener cuidado porque lo hace segun el Valor Unicode
+
+//centroamerica.sort()
